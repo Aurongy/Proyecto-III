9d18cad4b3735a1f870d0aca341d5273bb7f5610
Angel Aurelio <159669326+Aurongy@users.noreply.github.com>
2026-05-18 21:58:14 -0600

    Update NodoCabecera.java

--- Proyecto III/Proyecto III/src/NodoCabecera.java
+++ Proyecto III/Proyecto III/src/NodoCabecera.java
@@ -1,9 +1,8 @@
 public class NodoCabecera {
-
     int indice;
     NodoCabecera siguiente;
     VehiculoNodo acceso;
-
+    
     public NodoCabecera(int indice) {
         this.indice = indice;
     }
